5bf56a0a6f91e3fad6ce376fed422370d42bfa08
Fearn Liu <16191685+fearnliu@users.noreply.github.com>
2025-05-28 14:14:39 +0800

    the template for npm init

---
+++ .npm-init.js
@@ -0,0 +1,48 @@
+module.exports = {
+  "$schema": "https://json.schemastore.org/package.json",
+  "type": "module",
+  "name": "",
+  "version": "0.1.0",
+  "description": "",
+  "keywords": [],
+  "homepage": "",
+  "bugs": {
+    "url": ""
+  },
+  "license": "MPL-2.0",
+  "author": {
+    "name": "Fearn Liu",
+    "email": "16191685+fearnliu@users.noreply.github.com",
+    "url": "https://github.com/fearnliu"
+  },
+  "contributors": [],
+  "files": [
+    "index.js"
+  ],
+  "exports": {
+    ".": "./index.js"
+  },
+  "main": "index.js",
+  "bin": "",
+  "repository": {
+    "type": "git",
+    "url": ""
+  },
+  "scripts": {},
+  "config": {},
+  "dependencies": {},
+  "devDependencies": {},
+  "peerDependencies": {},
+  "peerDependenciesMeta": {},
+  "bundleDependencies": [],
+  "optionalDependencies": {},
+  "engines": {
+    "node": "",
+    "npm": ""
+  },
+  "devEngines": {},
+  "private": false,
+  "publishConfig": {
+    "access": "public"
+  }
+}
